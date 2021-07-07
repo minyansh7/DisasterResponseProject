a3682693c21ea184b1cb09ef8e65af89eeb2f0fd
Minyan <shminyan@gmail.com>
2021-07-08 00:45:16 +1000

    Add files via upload

--- process_data.py
+++ process_data.py
@@ -1,16 +1,46 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
+
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
+    messages = pd.read_csv('messages_filepath')
+    categories = pd.read_csv('categories_filepath')
+    #merge datasets
+    df = messages.merge(categories,how='outer',on=['id'])
+    return df
 
 def clean_data(df):
-    pass
+    #split the values in 'categories' column
+    #create a dataframe of the 36 individual category columns
+    #use the first row of categories dataframe to create column names
+    #use this row to extract a list of new column names
+    #take everything up to the second to last character of each string with slicing     
+    #rename columns of 'categories' with new column names
+    categories = df['categories'].str.split(';',expand=True)
+    row = categories.iloc[0]
+    category_colnames=list(map(lambda x: x[:-2], row))
+    categories.columns = category_colnames
+    #convert category values to just 0 or 1
+    #iterate through the category columns to keep only the last character of each string 
+    #use map() on the column result to apply lambda function over each element of the column
+    for column in categories:
+        categories[column]= categories[column].map(lambda x:x[-1:])
+        categories[column]= pd.to_numeric(categories[column].astype(str))
+    #drop the original categories column from 'df'
+    df=df.drop(['categories'],axis=1)
+    #concatenate df and categories dataframe
+    df = pd.concat([df,categories],axis=1)
+    # drop duplicates
+    df.drop_duplicates(keep='first', inplace=True)
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine('database_filename')
+    df.to_sql('database_filename', engine, index=False)
+    return df, database_filename
 
 
 def main():
